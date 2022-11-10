6ce0b8d9e918b31f8b9f810fc6ee68abd239a42e
kimhyunmin <twingfull@gmail.com>
2022-11-10 16:09:15 +0900

    회원가입 폼 만들고 제이쿼리로 줫같은것들 다 방지함

--- src/main/java/kr/ac/kopo/YourTrip/Util/PageUtil.java
+++ src/main/java/kr/ac/kopo/YourTrip/Util/PageUtil.java
@@ -1,9 +1,10 @@
 package kr.ac.kopo.YourTrip.Util;
 import javax.servlet.http.HttpServletRequest;
 public class PageUtil {
+    final  SysoutTester tester = new SysoutTester();
      public String prevPage (HttpServletRequest request){
         String prevPage = request.getHeader("referer");
-
+        tester.test(prevPage, getClass().getSimpleName());
         return prevPage;
     }
 }
